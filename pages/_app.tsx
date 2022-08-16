1fb0566a2d4014f4cc1de605d9903af1cfdd0680
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-08-16 07:17:08 -0500

    ADD: init proyect i love cats

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return <>
+    <Head>
+      <title>I Love Cats</title>
+      <meta name="description" content="The best cat app in the world" />
+      <link rel="icon" href="/cat.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
